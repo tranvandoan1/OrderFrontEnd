a348399bf643c008f52de4931c084061e617f08a
tranvandoan1 <tranlove24@gmail.com>
2022-06-12 23:41:31 +0700

    asdas

--- src/API/Users.js
+++ src/API/Users.js
@@ -1,29 +1,13 @@
 import { axiosClient } from "./link.js";
 
 const UserAPI = {
-    getAll() {
-        const url = `/user`;
-        return axiosClient.get(url);
-    },
-    get(id) {
-        const url = `/user/${id}`;
-        return axiosClient.get(url);
-    },
-    signup(user) {
-        const url = `/signup`;
-        return axiosClient.post(url, user);
-    },
-    signin(user) {
-        const url = `/signin`;
-        return axiosClient.post(url, user);
-    },
-    remove(id) {
-        const url = `/user/${id}`;
-        return axiosClient.delete(url);
-    },
-    upload(id, data) {
-        const url = `/user/${id}`;
-        return axiosClient.put(url, data);
-    },
+  signup(user) {
+    const url = `/signup`;
+    return axiosClient.post(url, user);
+  },
+  signin(user) {
+    const url = `/signin`;
+    return axiosClient.post(url, user);
+  },
 };
 export default UserAPI;
