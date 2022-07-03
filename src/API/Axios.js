8e157cea338f4b564cf242f2f94bfe4ae2d8b1ba
tranvandoan1 <tranlove24@gmail.com>
2022-07-03 21:52:37 +0700

    aassda

--- src/API/Axios.js
+++ src/API/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-    baseURL: "http://localhost:9000/api",
+    baseURL: "https://order-back.vercel.app/api",
     headers: {
         "Content-Type": "application/json",
     },
